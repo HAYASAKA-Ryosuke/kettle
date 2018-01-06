632debbe43a2724dc2c25c0f233ba92268be8459
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2018-01-07 06:15:23 +0900

    第二引数にresponseオブジェクトを持たせるようにした

--- kettle/application.py
+++ kettle/application.py
@@ -10,9 +10,9 @@ class Application:
     def __call__(self, environ, start_response):
         view, actions = self.router.search(environ.get('REQUEST_METHOD'), environ.get('PATH_INFO', '/'))
         if view is None:
-            response = Response('404 not found', 404)
+            response = Response().send('404 not found', 404)
         else:
-            response = view(Request(environ, actions))
+            response = view(Request(environ, actions), Response())
         status, headers, body = response.params()
         start_response(status, headers)
         return [body]
--- kettle/response.py
+++ kettle/response.py
@@ -2,10 +2,11 @@ class Response:
 
     STATUS = {200: '200 OK', 404: '404 Not Found', 400: '400 Bad Request'}
 
-    def __init__(self, body='', status=200, headers=[('Content-type', 'text/html; charset=UTF-8')]):
+    def send(self, body='', status=200, headers=[('Content-type', 'text/html; charset=UTF-8')]):
         self.status = self.STATUS.get(status, 200)
         self.headers = headers
         self.body = body
+        return self
 
     def params(self):
         if isinstance(self.body, str):
